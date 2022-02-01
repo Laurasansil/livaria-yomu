5becb67680dbdeeec9d4f32894af00619ed58029
Laura Silva <laura.silva@ifood.com.br>
2022-02-01 14:16:05 -0300

    Add user.json

--- fake-backend/handlers.ts
+++ fake-backend/handlers.ts
@@ -1,10 +1,26 @@
 import { rest } from "msw";
 import products from "./data/products.json";
+import user from "./data/user.json";
 
 export const handlers = [
   rest.get("https://backend.dev/products", (req, res, ctx) => {
-    console.log({ handlers });
     return res(ctx.json(products));
+  }),
+  rest.post("https://backend.dev/login", (req, res, ctx) => {
+    // @ts-ignore
+    const { email, password } = JSON.parse(req.body);
+    if (email == "laura@yomu.com" && password == "asdf") {
+      return res(ctx.json(user));
+    }
+    return res(
+      // Send a valid HTTP status code
+      ctx.status(401),
+      // And a response body, if necessary
+      ctx.json({
+        message: `Username or password is invalid`,
+        //fazer try catch no front end
+      })
+    );
   }),
 
   //     rest.post("/users/:id/messages", (req, res, ctx) => {
